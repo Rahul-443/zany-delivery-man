e75f0ba71acb43fb0f89137a70154307545dacc2
rahul369k <rg085425@gmail.com>
2021-12-08 15:36:48 +0530

    added transaction function

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import { JsSignatureProvider } from 'eosjs/dist/JsSignatureProvider';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth, signInAnonymously } from 'firebase/auth';
-import { firebaseConfig } from './config';
 import { getDatabase, ref, onValue } from 'firebase/database';
+import { firebaseConfig } from './config';
+import { pvk } from './config';
+import 'regenerator-runtime';
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
+const signatureProvider = new JsSignatureProvider([pvk]);
+const rpc = new JsonRpc('https://api.wax.alohaeos.com');
+const api = new Api({ rpc, signatureProvider });
 const scoreToZanyM = 0.1;
+let usersData;
 
 signInAnonymously(auth)
   .then(() => {
@@ -20,3 +26,55 @@ signInAnonymously(auth)
   .catch(error => {
     console.log(error);
   });
+
+onValue(
+  ref(database),
+  snapshot => {
+    if (snapshot !== null) {
+      usersData = snapshot;
+    }
+  },
+  error => {
+    console.log(error);
+  }
+);
+
+function transactAllUsers() {
+  if (usersData !== undefined) {
+    let userAddresses = Object.keys(usersData);
+    userAddresses.forEach(userAddress => {
+      let amt = (usersData[userAddress]['score'] * scoreToZanyM)
+        .toFixed(4)
+        .toString();
+      (async () => {
+        const result = await api.transact(
+          {
+            actions: [
+              {
+                account: 'metatoken.gm',
+                name: 'transfer',
+                authorization: [
+                  {
+                    actor: 'zanygumplays',
+                    permission: 'active'
+                  }
+                ],
+                data: {
+                  from: 'zanygumplays',
+                  to: userAddress,
+                  quantity: amt,
+                  memo: 'Thanks for Playing'
+                }
+              }
+            ]
+          },
+          {
+            blocksBehind: 3,
+            expireSeconds: 30
+          }
+        );
+        console.log(result);
+      })();
+    });
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = 'style-loader';
 
 const config = {
-  entry: './src/index.js',
+  entry: ['regenerator-runtime.js', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'public')
   },
