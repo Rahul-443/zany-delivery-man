506b46c0e0ebf33d4acd9ea453884799f8e85b92
rahul369k <rg085425@gmail.com>
2021-12-08 19:34:08 +0530

    added styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { Api, JsonRpc, RpcError } from 'eosjs';
-import { JsSignatureProvider } from 'eosjs/dist/JsSignatureProvider';
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import { firebaseConfig } from './config';
 import { pvk } from './config';
-import 'regenerator-runtime';
+import 'regenerator-runtime/runtime';
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -16,65 +16,89 @@ const database = getDatabase(app);
 const signatureProvider = new JsSignatureProvider([pvk]);
 const rpc = new JsonRpc('https://api.wax.alohaeos.com');
 const api = new Api({ rpc, signatureProvider });
+
+const secMain = document.querySelector('.section-main');
+const btnSend = document.querySelector('.send');
+let failedTrxList = [];
+
 const scoreToZanyM = 0.1;
+let userAddresses;
 let usersData;
 
 signInAnonymously(auth)
   .then(() => {
     console.log('Signed In');
+    onValue(
+      ref(database),
+      snapshot => {
+        if (snapshot !== null) {
+          usersData = snapshot.val();
+          console.log(usersData);
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
   })
   .catch(error => {
     console.log(error);
   });
 
-onValue(
-  ref(database),
-  snapshot => {
-    if (snapshot !== null) {
-      usersData = snapshot;
-    }
-  },
-  error => {
-    console.log(error);
-  }
-);
-
 function transactAllUsers() {
   if (usersData !== undefined) {
-    let userAddresses = Object.keys(usersData);
+    if (failedTrxList.length === 0) {
+      userAddresses = Object.keys(usersData);
+    }
     userAddresses.forEach(userAddress => {
+      let userName = userAddress.replace(/\_/g, '.');
       let amt = (usersData[userAddress]['score'] * scoreToZanyM)
         .toFixed(4)
         .toString();
-      (async () => {
-        const result = await api.transact(
-          {
-            actions: [
-              {
-                account: 'metatoken.gm',
-                name: 'transfer',
-                authorization: [
-                  {
-                    actor: 'zanygumplays',
-                    permission: 'active'
+      secMain.textContent = `Rewarding ${userName} with ${amt} Zany`;
+      try {
+        (async () => {
+          const result = await api.transact(
+            {
+              actions: [
+                {
+                  account: 'metatoken.gm',
+                  name: 'transfer',
+                  authorization: [
+                    {
+                      actor: 'zanygumplays',
+                      permission: 'active'
+                    }
+                  ],
+                  data: {
+                    from: 'zanygumplays',
+                    to: userName,
+                    quantity: `${amt} ZANY`,
+                    memo: 'Thanks for Coming by'
                   }
-                ],
-                data: {
-                  from: 'zanygumplays',
-                  to: userAddress,
-                  quantity: amt,
-                  memo: 'Thanks for Playing'
                 }
-              }
-            ]
-          },
-          {
-            blocksBehind: 3,
-            expireSeconds: 30
-          }
-        );
-        console.log(result);
-      })();
+              ]
+            },
+            {
+              blocksBehind: 3,
+              expireSeconds: 30
+            }
+          );
+          console.log(result);
+        })();
+      } catch (error) {
+        failedTrxList.push(userAddress);
+        console.log(`Caught Exception ${error}`);
+        if (error instanceof RpcError) {
+          console.log(JSON.stringify(error, null, 2));
+        }
+      }
     });
+    if (failedTrxList.length !== 0) {
+      userAddresses = failedTrxList;
+      transactAllUsers();
+    }
   }
 }
+
+btnSend.addEventListener('click', transactAllUsers);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = 'style-loader';
 
 const config = {
-  entry: ['regenerator-runtime.js', './src/index.js'],
+  entry: ['regenerator-runtime/runtime.js', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'public')
   },
